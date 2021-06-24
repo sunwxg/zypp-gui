7dd0b326e1c3dea9def8a12b133faef0771fa2fc
Xiaoguang Wang <sun.wxg@gmail.com>
2021-06-24 12:58:04 +0800

    Don't remove vendor folder when creating dist

--- build-aux/dist-vendor.sh
+++ build-aux/dist-vendor.sh
@@ -6,11 +6,11 @@ cd "$SOURCE_ROOT"
 mkdir "$DIST"/.cargo
 cargo vendor | sed 's/^directory = ".*"/directory = "vendor"/g' > $DIST/.cargo/config
 # Move vendor into dist tarball directory
-mv vendor "$DIST"
+cp -r vendor "$DIST"
 
 cd "$SOURCE_ROOT"/mod_repo
 mkdir "$DIST"/mod_repo/.cargo
 cargo vendor | sed 's/^directory = ".*"/directory = "vendor"/g' > $DIST/mod_repo/.cargo/config
 # Move vendor into dist tarball directory
 cp -r vendor "$DIST"
-mv vendor "$DIST"/mod_repo/
+cp -r vendor "$DIST"/mod_repo/
